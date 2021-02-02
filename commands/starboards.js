b753def0374d0ebb89ebc34c16b37b2de0251a8b
BobbyTheCatfish <alp6tree@gmail.com>
2021-02-02 10:02:31 -0500

    changed logic

--- commands/starboards.js
+++ commands/starboards.js
@@ -10,7 +10,7 @@ let starboards = async(msg) => await Module.db.guildconfig.getStarBoards(msg.gui
 let postToBoard = async(reaction, user, force=false) =>{
     let msg = reaction.message
     let guildBoards = await starboards(msg)
-    if(guildBoards.length > 0){
+    if(guildBoards?.length > 0){
         if(!guildBoards.includes({channel: msg.channel.id})){
             for(x of guildBoards){
                 if(!x.reactions.includes(reaction.emoji.name) && !x.reactions.includes(reaction.emoji.id) && !(x.main && force)) continue
@@ -22,7 +22,7 @@ let postToBoard = async(reaction, user, force=false) =>{
                     embed.addField('Channel', msg.channel).addField('Jump to post', msg.url).setTimestamp(msg.createdAt).setAuthor(msg.member.displayName, msg.author.avatarURL()).setFooter(reaction.emoji.name)
                     if(channel){
                         channel.send({embed})
-                        Module.db.guildconfig.saveSBMessage(msg)
+                        //Module.db.guildconfig.saveSBMessage(msg)
                     }
                     else u.errorHandler(`Starboard Send Error`, `Couldn't send to channel *${x.channel}* in guild *${msg.guild.name}*`)
                 }
@@ -32,13 +32,17 @@ let postToBoard = async(reaction, user, force=false) =>{
 }
 
 Module.addEvent('messageReactionAdd', async(reaction, user) =>{
-    if(reaction.message.author.bot) return
-    if(user.bot) return
-    if(!reaction.message.guild) return
-    if(reaction.message.createdTimestamp < (Date.now() - 3 * 24 * 60 * 60000)) return
-    let member = reaction.message.guild.members.cache.get(user.id)
-    if(member.hasPermission('MANAGE_GUILD') && reaction.emoji.name == '🌟') return await postToBoard(reaction, member, true)
-    else await postToBoard(reaction, user) 
+    if(!reaction.message?.author.bot){
+        if(!user.bot){
+            if(reaction.message.guild){
+                if(reaction.message.createdTimestamp > (Date.now() - 3 * 24 * 60 * 60000)){
+                    let member = reaction.message.guild.members.cache.get(user.id)
+                    if(member.hasPermission('MANAGE_GUILD') && reaction.emoji.name == '🌟') return await postToBoard(reaction, member, true)
+                    else await postToBoard(reaction, user) 
+                }
+            }
+        }
+    }
 })
 
 module.exports = Module;
