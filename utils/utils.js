79f7da18914035e6b0093a88c01adfe6984f5dec
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2021-10-12 19:29:02 -0400

    JSDoc and intellisense

--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,45 @@
-const Discord = require("discord.js")
+const {Util, WebhookClient, MessageButton, MessageActionRow, Message, MessageSelectMenu, MessageEmbed} = require("discord.js");
+const {GuildMember, GuildChannel, GuildEmoji, Guild} = require('discord.js')
 const config = require("../config/config.json")
-const colors = require('colors');
-const FileSync = require("lowdb/adapters/FileSync");
-const db = require(`../${config.db.model}`);
 const validUrl = require('valid-url');
-const errorLog = new Discord.WebhookClient({id: config.error.id, token: config.error.token})
-
+const jimp = require('jimp')
+const errorLog = new WebhookClient({id: config.error.id, token: config.error.token})
+const events = require('events'),
+    em = new events.EventEmitter()
 const Utils = {
-
+    /**
+     * @param {string} type The type of mod action
+     * @param {GuildMember} executor Who executed the command
+     * @param {GuildMember[]|GuildChannel[]|GuildEmoji[]} target Array of targets
+     * @param {string|number} [reason] Reason
+     * @returns {em} Emits a modEvent
+     */
+    modEvent: (type, executor, target, reason) => em.emit('modEvent', [type, executor, target, reason]),
+    
+    /**
+     * @param data Action Row Data
+     * @returns new MessageActionRow
+     */
+    actionRow: (data) => new MessageActionRow(data),
+    
+    /**
+     * @param data Message Button Data
+     * @returns new MessageButton
+     */
+    button: (data) =>  new MessageButton(data),
+    
+    /**
+     * @param data Message Select Menu Data
+     * @returns new MessageSelectMenu
+     */
+    selectMenu: (data) => new MessageSelectMenu(data),
+    
+    /**
+     * Deletes 1 or more messages
+     * @param {Message|Message[]} message Message Object(s)
+     * @param {number} t Time in ms
+     * @returns Message
+     */
     clean: async (message, t = 20000)=>{
         if(message.length > 1){
             setTimeout((message) =>{
@@ -22,41 +54,78 @@ const Utils = {
                     if (message.deletable && !message.deleted) message.delete();
             }, t, message);
             return Promise.resolve(message);
-        }
-        
+        }        
     },
-    confirmEmbed: async (message, promptEmbed, confirmEmbed, cancelEmbed, timeoutEmbed = Utils.embed().setTitle('Timed out').setDescription('You ran out of time!'), time = 6000)=>{
-            let msg = await (message.channel ? message.channel : message).send({embeds: [promptEmbed], allowedMentions: {parse: []}})
+    
+    /**
+     * @param {Message} message Message Object
+     * @param {MessageEmbed}promptEmbed Initial embed to send
+     * @param {MessageEmbed}confirmEmbed Embed to send if the user reacts with ✅
+     * @param {MessageEmbed}cancelEmbed Embed to send if the user reacts with 🛑
+     * @param {MessageEmbed}timeoutEmbed Embed to send if the user runs out of time
+     * @param {number} time Time in ms
+     * @returns boolean/null
+     */
+    confirmEmbed: async (message, promptEmbed, confirmEmbed, cancelEmbed, timeoutEmbed = embed().setTitle('Timed out').setDescription('You ran out of time!'), time = 6000)=>{
+            let msg = await message.channel.send({embeds: [promptEmbed], allowedMentions: {parse: []}})
             await msg.react('✅');
             await msg.react('🛑');
             
             const filter = (reaction, user) => ['✅', '🛑'].includes(reaction.emoji.name) && user.id === (message.author ? message.author : message).id;
-          
+            
             try {
-              const collected = await msg.awaitReactions({filter, max: 1, time, errors: ['time'] });
-              const reaction = collected.first();
-              if (reaction.emoji.name === '✅') {
+                const collected = await msg.awaitReactions({filter, max: 1, time, errors: ['time'] });
+                const reaction = collected.first();
+                if (reaction.emoji.name === '✅') {
                 msg.edit({embeds: [confirmEmbed]});
                 return true;
-              } else {
+                } else {
                 msg.edit({embeds: [cancelEmbed]});
                 return false;
-              }
+                }
             } catch(error) {
-              msg.edit({embes: [timeoutEmbed]});
-              return null
+                msg.edit({embeds: [timeoutEmbed]});
+                return null
             }
     },
-
-    embed: (data) => new Discord.MessageEmbed(data).setColor(config.color),
     
-    escape: (text, options = {}) => Discord.escapeMarkdown(text, options),
-
+    /**
+     * @param data Message Embed Data
+     * @returns {MessageEmbed}new MessageEmbed
+     */
+    embed: (data) => new MessageEmbed(data).setColor(config.color),
+    
+    /**
+     * @param {string} text Text to escape
+     * @param {{}} options EscapeMarkdownOptions
+     * @returns {string} Escaped Text
+     */
+    escape: (text, options = {}) => Util.escapeMarkdown(text, options),
+    
+    /**
+     * @param {string} txt Text to escape
+     * @returns {string} Escaped Text
+     */
     escapeText: (txt) => txt.replace(/(\*|_|`|~|\\|\|)/g, '\\$1'),
-
-    getMention: async (message, parse=false, getMember = true) =>{
+    
+    /**
+     * @returns {function} null function
+     */
+    noop: () => {},
+    
+    /**
+     * @param {any[]} array Array
+     * @returns {any} Random entry from the array
+     */
+    rand: (array) => array[Math.floor(Math.random() * array.length)],
+    
+    /**
+     * Kinda borked, I wouldnt use tbh
+     */
+    getMention: async (message, parse='', getMember = true)=>{
         try
         {
+            message
             if(!parse){
                 let {suffix} = await Utils.parse(message)
                 if (message.guild){
@@ -90,63 +159,25 @@ const Utils = {
             }
         }catch(error) {return null;}
     },
-    getMentions: async(msg, member = false) =>{
+    
+    /**
+     * Kinda borked, I wouldn't use tbh
+     */
+    getMentions: async (msg, member = false)=>{
         let users = Utils.parse(msg).suffix.match(/<@!?[0-9]*>/g).join('\n').replace(/[^0-9\n]/g, '').split('\n')
         let userArray = []
-        if(member) for(u of users) userArray.push(msg.guild.members.cache.get(u))
-        else for (u of users) userArray.push(msg.client.users.cache.get(u))
+        if(member) for(let u of users) userArray.push(msg.guild.members.cache.get(u))
+        else for (let u of users) userArray.push(msg.client.users.cache.get(u))
         return userArray
     },
-    noop: () => {},
-
-    time: ()=> {
-        let date = new Date
-        let m = date.getMonth()+1, d = date.getDate(), y = date.getFullYear(), h = date.getHours(), mi = date.getMinutes(), s = date.getSeconds()
-        if(m < 10) m = '0'+m
-        if(d < 10) d = '0'+d
-        if(h < 10) h = '0'+h
-        if(mi < 10) mi = '0'+mi
-        if(s < 10) s = '0'+s
-        let give = `${m}/${d}/${y} @ ${h}:${m}:${s}`
-        return `[${give}] `.magenta
-    },
-
-    paginator: async (message, pager, elements, page = 0, perPage = 1) =>{
-        try{
-            let totalPages = Math.ceil(elements.length / perPage);
-            if (totalPages > 1){
-                let embed = pager(elements, page, message).setFooter(`Page ${page + 1} / ${totalPages}. React with ⏪ and ⏩ to navigate.`);
-                let m = await message.channel.send({embeds: [embed]});
-                await m.react("⏪");
-                await m.react("⏩");
-                let reactions;
-
-                do{
-                    reactions = await m.awaitReactions({filter: (reaction, user) => (user.id == message.author.id) && ["⏪", "⏩"].includes(reaction.emoji.name), time: 300000, max: 1 });
-                    if (reactions.size > 0){
-                        let react = reactions.first().emoji.name;
-                        if (react == "⏪") page--;
-                        else if (react == "⏩") page++;
-                        if (page < 0 || page >= totalPages) page = (page + totalPages) % totalPages;
-                        reactions.first().remove(message.author.id);
-                        embed = pager(elements, page, message).setFooter(`Page ${page + 1} / ${totalPages}. React with ⏪ and ⏩ to navigate.`);
-                        m = await m.edit({embeds: [embed]});
-                    }
-                } while (reactions.size > 0);
-
-                embed.setFooter(`Page ${page + 1} / ${totalPages}`);
-                m.edit({embed: [embed]});
-                for (const [rid, r] of m.reactions.cache){
-                    if (!r.me) continue;
-                    else r.remove();
-                }
-            } else await message.channel.send({embeds: [pager(elements, page, message)]});
-        } catch(e) { Utils.alertError(e, message); }
-    },
-    parse: async (msg) => {
+    
+    /**
+     * @param {Message} msg Message Object
+     * @returns {Promise<{command: string, suffix:string, params: []}>} Parsed message content
+     */
+    parse: async (msg)=>{
         try {
-            
-          let prefix = await Utils.prefix(msg),
+            let prefix = await Utils.prefix(msg),
             message = msg.content,
             parse;
             if (msg.author.bot) parse = null;
@@ -162,13 +193,18 @@ const Utils = {
                 };
             } else return null;
         } catch(e) {
-          Utils.errorHandler(e, msg.content);
-          return null;
+            Utils.errorHandler(e, msg);
+            return null;
         }
     },
-    prefix: async (msg) => {
+    
+    /**
+     * @param {Message} msg Message Object
+     * @returns {Promise<string>} Prefix
+     */
+    prefix: async (msg)=>{
         try {
-            if(msg.channel.parentId == '813847559252344862') return '>'
+            if(msg.channel?.parentId == '813847559252344862') return '>'
             else if (msg.guild) return await msg.client.db.guildconfig.getPrefix(msg.guild.id);
             else return config.prefix;
         } catch(e) {
@@ -176,61 +212,133 @@ const Utils = {
             return config.prefix;
         }
     },
-    path: async (...segments) => {
+    
+    /**
+     * @param {...string} segments
+     * @returns {Promise<string>} idk the path? i've never used this before
+     */
+    path: async (...segments)=>{
         const path = require("path");
         return path.resolve(path.dirname(require.main.filename), ...segments);
     },
-    properCase: (txt) => txt.split(" ").map(word => (word[0].toUpperCase() + word.substr(1).toLowerCase())).join(" "),
     
-    rand: (array) => array[Math.floor(Math.random() * array.length)],
+    /**
+     * @param {string} txt Text to put into proper case
+     * @param {boolean} replace Replace _ with spaces
+     * @returns {string} Proper case string
+     */
+    properCase: (txt, replace = false)=>{
+        if(!txt) return txt
+        if(replace) txt = txt.replace(/_/g, ' ')
+        return txt.split(" ").map(word => (word[0].toUpperCase() + word.substr(1).toLowerCase())).join(" ")
+    },
     
-    react: async (message, reactions) => {
+    /**
+     * Kidna pointless but helps in low-traffic scenarios
+     * @param {Message} message Message Object
+     * @param {string} reactions Reaction ID/name
+     * @returns {Promise<null>}
+     */
+    react: async (message, reactions)=>{
         let i = 0
         let x = setInterval(()=>{
             message.react(reactions[i])
             i++
             if(i == reactions.length) clearInterval(x)
         }, 1500)
+        return null
     },
-
-    validUrl: async (message) =>{
-        if(validUrl.isUri(message)) return true
-        else return undefined
+    
+    /**
+     * @param {Message} msg Message to reply to
+     * @param {string} content What to reply with
+     * @param {boolean} clean Use u.clean or not
+     * @param {boolean} mention Mention the user
+     */
+    reply: async(msg, content, clean = false, mention = false,)=>{
+        if(!msg) return Utils.errorHandler(new Error('u.reply needs a message object'), `Reply content: ${content}`)
+        else msg.reply({content, allowedMentions: {repliedUser: mention}, failIfNotExists: false}).then(m => {if(clean) Utils.clean(m)})
     },
-    botSpam: async (msg) =>{
+    
+    /**
+     * @param {string} txt text to verify
+     * @returns {boolean} is link or not
+     */
+    validUrl: (txt)=>{
+        if(validUrl.isWebUri(txt)) return true
+        else return false
+    },
+    
+    /**
+     * @param {string} image Image URL
+     * @returns {Promise<boolean>} is image or not
+     */
+    validImage: async(image)=>{
+        try{
+            await jimp.read(image)
+            return true
+        } catch{
+            return false
+        }
+    },
+    
+    /**
+     * @param {Message} msg Message Object
+     * @returns {Promise<GuildChannel>} Botspam or current channel
+     */
+    botSpam: async (msg)=>{
         if(!msg.guild) return msg.channel
         let channel = await msg.client.db.guildconfig.getBotLobby(msg.guild.id)
-        return channel ?? msg.channel
+        return msg.client.channels.cache.get(channel) ?? msg.channel
     },
-    errorChannel: async(msg) =>{
+    
+    /**
+     * @param {Message} msg Message Object
+     * @returns {Promise<GuildChannel>} error channel or current channel
+     */
+    errorChannel: async (msg)=>{
         if(!msg.guild) return msg.channel
         let channel = await msg.client.db.guildconfig.getErrorChannel(msg.guild.id)
         return msg.client.channels.cache.get(channel) ?? msg.channel
     },
-    errorHandler: async (error, msg = null) => {
-        if (!error) return;
-        console.error(Date());
-        let embed = Utils.embed().setTitle(error.name);
     
-        if (msg instanceof Discord.Message) {
-          console.error(`${msg.author.username} in ${(msg.guild ? `${msg.guild.name} > ${msg.channel.name}` : "DM")}: ${msg.cleanContent}`);
-          const client = msg.client;
-          msg.channel.send("I've run into an error. I've let my devs know.").then(Utils.clean);
-          embed.addField("User", msg.author.username, true).addField("Location", (msg.guild ? `${msg.guild.name} > ${msg.channel.name}` : "DM"), true).addField("Command", msg.cleanContent ?? "`undefined`", true);
-        } else if (typeof msg === "string") {
-          console.error(msg);
-          embed.addField("Message", msg.replace(/\/Users\/bobbythecatfish\/Desktop\//gi, ''));
-        }
-    
-        console.trace(error);
-    
-        let stack = (error.stack ? error.stack : error.toString());
-        if (stack.length > 1024) stack = stack.slice(0, 1000);
-    
-        embed.addField("Error", stack.replace(/\/Users\/bobbythecatfish\/Desktop\//gi, ''));
-        errorLog.send({embeds: [embed]});
+    /**
+     * @param {error} error error
+     * @param {Message} msg Message Object
+     * @returns Handles the error (don't worry about it)
+     */
+    errorHandler: async (error, msg = null)=>{
+        try{
+
+            if (!error) return;
+            console.error(Date());
+            let embed = Utils.embed().setTitle(error.name);
+        
+            if (msg instanceof Message) {
+                console.error(`${msg.author.username} in ${(msg.guild ? `${msg.guild.name} > ${(msg.channel).name}` : "DM")}: ${msg.cleanContent}`);
+                const client = msg.client;
+                msg.channel.send("I've run into an error. I've let my devs know.").then(Utils.clean);
+                embed.addField("User", msg.author.username, true).addField("Location", (msg.guild ? `${msg.guild.name} > ${(msg.channel).name}` : "DM"), true).addField("Command", msg.cleanContent ?? "`undefined`", true);
+            } else if (typeof msg === "string") {
+                console.error(msg);
+                embed.addField("Message", msg.replace(/\/Users\/bobbythecatfish\/Downloads\//gi, ''));
+            }
+        
+            console.trace(error);
+        
+            let stack = (error.stack ? error.stack : error.toString());
+            if (stack.length > 1024) stack = stack.slice(0, 1000);
+        
+            embed.addField("Error", stack.replace(/\/Users\/bobbythecatfish\/Downloads\//gi, ''));
+            return errorLog.send({embeds: [embed]});
+        } catch(e){console.log(e)}
     },
-    encodeLogEvents: (flags) => {
+    
+    /**
+     * @param {{category: string, int: number}[]} flags Array of flag categories and ints
+     * @returns {number} encoded number
+     */
+    encodeLogEvents: (flags)=>{
         let reduce = function(flags) {
             let numbers =  flags.map(f => f.int)
             let reduced = numbers.length > 1 ? numbers.reduce(function(a, b){return a+b}) : numbers
@@ -246,39 +354,13 @@ const Utils = {
         let role = reduce(flags.filter(f => f.category == 'role'))
         return channel + message + emoji + member + other + server + role
     },
-    decodeLogEvents: async(guild) =>{
-        let events = [
     
-            ['Channel Created', 1, 'channel'],
-            ['Channel Deleted', 2, 'channel'],
-            ['Channel Updated', 3, 'channel'],
-    
-            ['Message Delete', 1, 'message'],
-            ['Messages Bulk Deleted', 2, 'message'],
-            ['Message Pinned', 3, 'message'],
-    
-            ['Emoji Created', 1, 'emoji'],
-            ['Emoji Deleted', 2, 'emoji'],
-            ['Emoji Updated', 3, 'emoji'],
-    
-            ['Member Joined', 1, 'member'],
-            ['Member Left', 2, 'member'],
-            ['Member Updated', 3, 'member'],
-    
-            ['Member Banned', 1, 'other'],
-            ['Member Unbanned', 2, 'other'],
-            ['Inegrations Updated', 3, 'other'],
-    
-            ['Invite Created', 2, 'server'],
-            ['Invite Deleted', 3, 'server'],
-            ['Server Updated', 1, 'server'],
-    
-            ['Role Created', 1, 'role'],
-            ['Role Deleted', 2, 'role'],
-            ['Role Updated', 3, 'role'],
-    
-            ['Enable All', 7, 'all'] //777777
-    ]
+    /**
+     * @param {Guild} guild Guild Object
+     * @returns {Promise<string[]>} Array of flag names
+     */
+    decodeLogEvents: async (guild)=>{
+        let events = require('../jsons/events.json').events
         let decrypt = function(int, category){
             let filtered = events.filter(f => f[2] == category)
             if(int == 0) return []
