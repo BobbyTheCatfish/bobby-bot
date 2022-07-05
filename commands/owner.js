d70877d4717cc0f9be8cfca3ef010273ac3c41ee
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2022-07-05 13:18:04 -0400

    add more maintenance commands

--- commands/owner.js
+++ commands/owner.js
@@ -2,6 +2,7 @@ const Augur = require('augurbot'),
   u = require('../utils/utils'),
   nodemailer = require('nodemailer'),
   fs = require('fs'),
+  path = require('path'),
   spawn = require('child_process').spawn;
 const Module = new Augur.Module();
 
@@ -313,6 +314,7 @@ Module.addCommand({ name: "pingeveryone",
   },
 })
 
+// maintenance
 .addCommand({ name: "reload",
   category: "Bot Admin",
   hidden: true,
@@ -322,7 +324,6 @@ Module.addCommand({ name: "pingeveryone",
   info: "Use the command without a suffix to reload all command files.\n\nUse the command with the module name (including the `.js`) to reload a specific file.",
   process: (msg, suffix) => {
     u.clean(msg);
-    const path = require("path");
     const files = (suffix ? suffix.split(" ") : fs.readdirSync(path.resolve(__dirname)).filter(file => file.endsWith(".js")));
 
     for (const file of files) {
@@ -343,7 +344,6 @@ Module.addCommand({ name: "pingeveryone",
     u.clean(msg);
     msg.react("👌");
     if (suffix) {
-      const path = require("path");
       const files = suffix.split(" ").filter(f => f.endsWith(".js"));
       for (const file of files) {
         delete require.cache[require.resolve(path.dirname(require.main.filename), file)];
@@ -353,7 +353,85 @@ Module.addCommand({ name: "pingeveryone",
     }
   },
 })
+.addCommand({ name: "gotobed",
+  category: "Bot Admin",
+  hidden: true,
+  aliases: ["q", "restart"],
+  onlyOwner: true,
+  process: async function(msg) {
+    try {
+      await msg.react("🛏");
 
+      const files = fs.readdirSync(path.resolve(process.cwd(), "./commands")).filter(f => f.endsWith(".js"));
+
+      for (const file of files) {
+        Module.client.moduleHandler.unload(path.resolve(process.cwd(), "./commands/", file));
+      }
+
+      if (msg.client.shard) {
+        msg.client.shard.broadcastEval("this.destroy().then(() => process.exit())");
+      } else {
+        await msg.client.destroy();
+        process.exit();
+      }
+    } catch (e) { u.errorHandler(e, msg); }
+  }
+})
+.addCommand({ name: "ping",
+  category: "Bot Admin",
+  description: "Check bot ping.",
+  hidden: true,
+  process: (msg) => {
+    msg.channel.send('Pinging...').then(sent => {
+      sent.edit(`Pong! Took ${sent.createdTimestamp - (msg.editedTimestamp ? msg.editedTimestamp : msg.createdTimestamp)}ms`).then(u.clean);
+    });
+  }
+})
+.addCommand({ name: "pulse",
+  category: "Bot Admin",
+  hidden: true,
+  onlyOwner: true,
+  description: "Check the bot's heartbeat",
+  process: async function(msg) {
+    try {
+      const client = msg.client;
+
+      const embed = u.embed()
+      .setAuthor(client.user.username + " Heartbeat", client.user.displayAvatarURL()).setTimestamp();
+
+      if (client.shard) {
+        let guilds = await client.shard.fetchClientValues('guilds.cache.size');
+        guilds = guilds.reduce((prev, val) => prev + val, 0);
+        let channels = client.shard.fetchClientValues('channels.cache.size');
+        channels = channels.reduce((prev, val) => prev + val, 0);
+        let mem = client.shard.broadcastEval("Math.round(process.memoryUsage().rss / 1024 / 1000)");
+        mem = mem.reduce((t, c) => t + c);
+        embed
+        .addField("Shards", `Id: ${client.shard.id}\n(${client.shard.count} total)`, true)
+        .addField("Total Bot Reach", `${guilds} Servers\n${channels} Channels`, true)
+        .addField("Shard Uptime", `${Math.floor(client.uptime / (24 * 60 * 60 * 1000))} days, ${Math.floor(client.uptime / (60 * 60 * 1000)) % 24} hours, ${Math.floor(client.uptime / (60 * 1000)) % 60} minutes`, true)
+        .addField("Shard Commands Used", `${client.commands.commandCount} (${(client.commands.commandCount / (client.uptime / (60 * 1000))).toFixed(2)}/min)`, true)
+        .addField("Total Memory", `${mem}MB`, true);
+
+        msg.reply({ embeds: [embed] });
+      } else {
+        const uptime = process.uptime();
+        embed
+        .addField("Uptime", `Discord: ${Math.floor(client.uptime / (24 * 60 * 60 * 1000))} days, ${Math.floor(client.uptime / (60 * 60 * 1000)) % 24} hours, ${Math.floor(client.uptime / (60 * 1000)) % 60} minutes\nProcess: ${Math.floor(uptime / (24 * 60 * 60))} days, ${Math.floor(uptime / (60 * 60)) % 24} hours, ${Math.floor(uptime / (60)) % 60} minutes`, true)
+        .addField("Reach", `${client.guilds.cache.size} Servers\n${client.channels.cache.size} Channels\n${client.users.cache.size} Users`, true)
+        .addField("Commands Used", `${client.commands.commandCount} (${(client.commands.commandCount / (client.uptime / (60 * 1000))).toFixed(2)}/min)`, true)
+        .addField("Memory", `${Math.round(process.memoryUsage().rss / 1024 / 1000)}MB`, true);
+
+        msg.reply({ embeds: [embed] });
+      }
+    } catch (e) { u.errorHandler(e, msg); }
+  }
+})
+.setInit(async () => {
+  try {
+    Module.client.emit("loadConfig");
+  } catch (error) { u.errorHandler(error, "botAdmin Load"); }
+})
 // Add DBs on ready and create
 .addEvent('ready', async () => {
   Module.client.user.setActivity("Bobbby's screams coming from the basement because i'm torturing him 🙂🎉", { type: "LISTENING" });
